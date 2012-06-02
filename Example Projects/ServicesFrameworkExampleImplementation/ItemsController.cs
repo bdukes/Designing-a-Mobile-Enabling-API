1853e421460f8ff81a896f99e5a9e4feb1a3ab0d
Brian Dukes <bdukes@engagesoftware.com>
2012-06-02 14:46:27 -0500

    Fix controller stuff
    Don't restrict to GET
    Correct namespace

--- Example Projects/ServicesFrameworkExampleImplementation/ItemsController.cs
+++ Example Projects/ServicesFrameworkExampleImplementation/ItemsController.cs
@@ -9,7 +9,7 @@
 // CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 // DEALINGS IN THE SOFTWARE.
 
-namespace E
+namespace Engage.Dnn.ServicesFrameworkExampleImplementation
 {
     using System.Web.Mvc;
 
