75a815653fe1269d66d708a379bd4377ed976d03
Brian Dukes <bdukes@engagesoftware.com>
2013-03-23 19:35:36 -0500

    Create Windows Phone 8 app

---
+++ Example Projects/ServicesFrameworkWindowsPhone8Client/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ServicesFrameworkWindowsPhone8Client.Resources;
+
+namespace ServicesFrameworkWindowsPhone8Client
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
