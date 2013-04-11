12091e2c62be7dd6c6679291a16e85be1cd33a0c
Brian Dukes <bdukes@engagesoftware.com>
2013-04-10 23:56:09 -0500

    Update Controller example for Web API

---
+++ Example Projects/ServicesFrameworkExampleImplementation/ExampleController.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="ItemsController.cs" company="Engage Software">
+// EngageServicesFrameworkExampleImplementation
+// Copyright (c) 2004-2013
+// by Engage Software ( http://www.engagesoftware.com )
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+
+namespace Engage.Dnn.ServicesFrameworkExampleImplementation
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    using DotNetNuke.Web.Api;
+
+    public class ExampleController : DnnApiController
+    {
+        [AllowAnonymous]
+        public HttpResponseMessage GetHello()
+        {
+            var data = new { number = 1, text = "Hi!" };
+            return this.Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+    }
+}
